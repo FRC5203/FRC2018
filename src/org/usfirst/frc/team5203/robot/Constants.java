f4f11f96f0c1100161de0aaf82f10e6cf4b21b16
Meridian Robotics <meridian5203@gmail.com>
2018-01-16 20:38:38 -0500

    Initial Commit

---
+++ src/org/usfirst/frc/team5203/robot/Constants.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team5203.robot;
+
+import com.ctre.CANTalon;
+
+public class Constants {
+
+CANTalon frontLeft = new CANTalon(1);
+CANTalon rearLeft = new CANTalon(2);
+CANTalon frontRight = new CANTalon(3);
+CANTalon rearRight = new CANTalon(4);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
